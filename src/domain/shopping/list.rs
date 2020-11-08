09030afdadbf77a3ab34fe693c36e605efb15869
Narciso Benigno <narciso.benigno@indebted.co>
2020-11-08 22:47:43 +1100

    feat(domain): rename a list

--- src/domain/shopping/list.rs
+++ src/domain/shopping/list.rs
@@ -9,11 +9,21 @@ pub struct List {
 
 impl List {
     fn create(&self, event: CustomerCreateList) -> Result<Vec<ListEvent>, AggregateError> {
-        let event = ListEvent::CustomerCreatedList(CustomerCreatedList {
+        Ok(vec![ListEvent::CustomerCreatedList(CustomerCreatedList {
             id: event.id,
             name: event.name,
-        });
-        Ok(vec![event])
+        })])
+    }
+
+    fn rename(&self, event: CustomerRenameList) -> Result<Vec<ListEvent>, AggregateError> {
+        if event.new == self.name {
+            return Ok(vec![]);
+        }
+
+        Ok(vec![ListEvent::CustomerRenamedList(CustomerRenamedList {
+            former: self.name.to_string(),
+            new: event.new,
+        })])
     }
 }
 
@@ -29,6 +39,23 @@ impl Default for List {
             id: "".to_string(),
             name: "".to_string(),
         }
+    }
+}
+
+trait ListEventApply<E> {
+    fn apply(&mut self, event: E);
+}
+
+impl ListEventApply<CustomerCreatedList> for List {
+    fn apply(&mut self, event: CustomerCreatedList) {
+        self.id = event.id;
+        self.name = event.name
+    }
+}
+
+impl ListEventApply<CustomerRenamedList> for List {
+    fn apply(&mut self, event: CustomerRenamedList) {
+        self.name = event.new
     }
 }
 
@@ -59,23 +86,6 @@ impl DomainEvent<List> for ListEvent {
     }
 }
 
-trait ListEventApply<E> {
-    fn apply(&mut self, event: E);
-}
-
-impl ListEventApply<CustomerCreatedList> for List {
-    fn apply(&mut self, event: CustomerCreatedList) {
-        self.id = event.id;
-        self.name = event.name
-    }
-}
-
-impl ListEventApply<CustomerRenamedList> for List {
-    fn apply(&mut self, event: CustomerRenamedList) {
-        self.name = event.new
-    }
-}
-
 #[derive(Serialize, Deserialize)]
 pub struct CustomerCreateList {
     id: String,
@@ -88,6 +98,17 @@ impl Command<List, ListEvent> for CustomerCreateList {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct CustomerRenameList {
+    new: String,
+}
+
+impl Command<List, ListEvent> for CustomerRenameList {
+    fn handle(self, list: &List) -> Result<Vec<ListEvent>, AggregateError> {
+        list.rename(self)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use cqrs_es::test::TestFramework;
@@ -97,18 +118,53 @@ mod tests {
     type ShoppingListSuite = TestFramework<shopping::list::List, shopping::list::ListEvent>;
 
     #[test]
-    fn it_adds_items() {
-        let expected =
-            shopping::list::ListEvent::CustomerCreatedList(shopping::list::CustomerCreatedList {
-                id: "list-uuid-1".to_string(),
-                name: "List name".to_string(),
-            });
+    fn it_creates_a_list() {
         ShoppingListSuite::default()
             .given_no_previous_events()
             .when(shopping::list::CustomerCreateList {
                 id: "list-uuid-1".to_string(),
                 name: "List name".to_string(),
             })
-            .then_expect_events(vec![expected]);
+            .then_expect_events(vec![shopping::list::ListEvent::CustomerCreatedList(
+                shopping::list::CustomerCreatedList {
+                    id: "list-uuid-1".to_string(),
+                    name: "List name".to_string(),
+                },
+            )]);
+    }
+
+    #[test]
+    fn it_renames_a_list() {
+        ShoppingListSuite::default()
+            .given(vec![shopping::list::ListEvent::CustomerCreatedList(
+                shopping::list::CustomerCreatedList {
+                    id: "list-uuid-1".to_string(),
+                    name: "List name".to_string(),
+                },
+            )])
+            .when(shopping::list::CustomerRenameList {
+                new: "New list name".to_string(),
+            })
+            .then_expect_events(vec![shopping::list::ListEvent::CustomerRenamedList(
+                shopping::list::CustomerRenamedList {
+                    former: "List name".to_string(),
+                    new: "New list name".to_string(),
+                },
+            )]);
+    }
+
+    #[test]
+    fn it_does_not_rename_when_got_same_name() {
+        ShoppingListSuite::default()
+            .given(vec![shopping::list::ListEvent::CustomerCreatedList(
+                shopping::list::CustomerCreatedList {
+                    id: "list-uuid-1".to_string(),
+                    name: "List name".to_string(),
+                },
+            )])
+            .when(shopping::list::CustomerRenameList {
+                new: "List name".to_string(),
+            })
+            .then_expect_events(vec![]);
     }
 }
